a3311443f035efa90a6a3a0588b2fc35677c01da
MiriamJB <mjb376@ensign.edu>
2024-01-23 13:46:49 -0700

    Connect to Redis

--- index.js
+++ index.js
@@ -1,25 +1,36 @@
+/* COMMANDS:
+    npm run dev = run this program with automatic restarts on updates
+    node index.js = run this program
+    docker-compose up -d = runs docker in the background (deamon) & rebuilds containers (specified in docker-compose.yml)
+*/
+
 //express is a framework for node.js
 //it makes APIs - connects frontend to database backend
+//require = import (import needs more steps to work)
 const express = require('express');
+const Redis = require('redis');
+
+const redisClient = Redis.createClient({
+    url : `redis://localhost:6379`
+});
 
 const app = express(); //creates an express application
-app.listen(3000); //listens for web requests from frontend on port 3000 (and doesn't stop)
+const port = 3000;
+app.listen(port, ()=>{
+    redisClient.connect(); //connects to the redis database
+    console.log(`API is listening on port ${port}`);
+}); //listens for web requests from frontend on port 3000 (and doesn't stop)
 
-//boxes database
-const boxes = [
-    {boxId: 1},
-    {boxId: 2},
-    {boxId: 3},
-    {boxId: 4}
-];
-
-/*Using the API:
+/* Using an API:
     1. URL
     2. function to return boxes
     req = resquest from the browser
-    res = response to the bowser 
+    res = response to the bowser    
 */
+
 app.get('/boxes', (req,res) => {
+    let boxes = redisClient.json.get('boxes', {path:'$'}); //gets boxes
+
     //send boxes to browser
     res.send(JSON.stringify(boxes)); //convert boxes to a string
 }); //return boxes to user
