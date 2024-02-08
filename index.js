382e2cece5cd7272b8b6d1842e86c32938847b7e
MiriamJB <mjb376@ensign.edu>
2024-02-07 20:03:57 -0700

    Add post customer

--- index.js
+++ index.js
@@ -2,7 +2,7 @@
     npm run dev = run this program with automatic restarts on updates
     node index.js = run this program
     docker-compose up -d = runs docker in the background (deamon) & rebuilds containers (specified in docker-compose.yml)
-    http://localhost:3000/boxes - address it is running on
+    http://localhost:3001/boxes - address it is running on
 */
 
 //require = import (import needs more steps to work)
@@ -11,15 +11,21 @@
 const express = require('express');
 const Redis = require('redis');
 const bodyParser = require('body-parser');
+const cors = require('cors');
+
+const options = {
+    origin: 'http://localhost:3000' //allow frontend to call this URL for the backend
+}
 
 const redisClient = Redis.createClient({
     url : `redis://localhost:6379`
 });
 
 const app = express(); //creates an express application
-const port = 3000;
+const port = 3001;
 
 app.use(bodyParser.json());
+app.use(cors(options)); //allow frontend to call backend
 
 app.listen(port, ()=>{
     redisClient.connect(); //connects to the redis database
@@ -35,7 +41,7 @@ app.listen(port, ()=>{
 
 app.get('/boxes', async (req,res) => {
     let boxes = await redisClient.json.get('boxes', {path:'$'}); //gets boxes; without 'await' it returns a Promise to the frontend (that's bad)
-    res.json(boxes); //convert boxes to a string and send to browser
+    res.json(boxes[0]); //convert boxes to a string and send to browser
 }); //return boxes to user
 
 app.post('/boxes', async (req,res) => {
@@ -43,6 +49,20 @@ app.post('/boxes', async (req,res) => {
     newBox.id = parseInt(await redisClient.json.arrLen('boxes', '$')) + 1;
     await redisClient.json.arrAppend('boxes', '$', newBox); //saves the new JSON in Redis
     res.json(newBox);
+});
+
+app.post('/customer', async (req,res) => {
+    const newCustomer = req.body;
+    let responseStatus = (newCustomer.name) ? 200 : 400;
+    if (responseStatus === 200) {
+        newCustomer.id = parseInt(await redisClient.json.arrLen('customer', '$')) + 1;
+        await redisClient.json.arrAppend('customer', '$', newCustomer);
+    } else {
+        res.status(responseStatus);
+        res.send("Error: cannot create customer due to missing feilds");
+    }
+    //res.status(responseStatus).send();
+    res.json(newCustomer);
 });
 
 console.log("Hello World");
